69294287e6401b537ac0910b4a4deb2dd172c40f
Lucas Sousa <lucassousagth@gmail.com>
2024-01-27 11:36:17 -0300

    validação senha pt1

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 function validation(event){
+
     event.preventDefault(event)
+    
     var regex=/[^a-zA-Z]/
     var nome=document.getElementById("name").value
-    if(regex.test(nome)){
-        alert("O nome só pode conter letras!")
-
+    var senha=document.getElementById("password").value
+    //o if está rodando mesmo quando a senha <6
+    if(senha>=6){
+        if(regex.test(senha)){
+            alert("ok")
+            document.getElementById("password").style.display="none"
+        }
+        else{
+            document.getElementById("password").style.display="block"
+            document.getElementById("password").style.border="2px solid rgba(211, 67, 67, 0.729)"
+        }
     }
     else{
-        alert("O nome está correto")
+        alert("aaaa")
     }
+   if(regex.test(nome)){
+        document.getElementById("errnome").style.display="block";
+    }
+    else{
+        document.getElementById("errnome").style.display="none";
+        alert("O nome está correto")
+
+    }
+
 }
