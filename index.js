10535fb0e20d062e92777dc17757a99795953ac7
Lucas Sousa <lucassousagth@gmail.com>
2024-01-28 12:23:39 -0300

    validação senha funcionando

--- index.js
+++ index.js
@@ -2,23 +2,11 @@ function validation(event){
 
     event.preventDefault(event)
     
-    var regex=/[^a-zA-Z]/
+    var regex=/^(?=.*\d)(?=.*[!@#$%^&*])[a-zA-Z\d!@#$%^&*]/;
     var nome=document.getElementById("name").value
     var senha=document.getElementById("password").value
-    //o if está rodando mesmo quando a senha <6
-    if(senha>=6){
-        if(regex.test(senha)){
-            alert("ok")
-            document.getElementById("password").style.display="none"
-        }
-        else{
-            document.getElementById("password").style.display="block"
-            document.getElementById("password").style.border="2px solid rgba(211, 67, 67, 0.729)"
-        }
-    }
-    else{
-        alert("aaaa")
-    }
+
+
    if(regex.test(nome)){
         document.getElementById("errnome").style.display="block";
     }
@@ -26,6 +14,26 @@ function validation(event){
         document.getElementById("errnome").style.display="none";
         alert("O nome está correto")
 
+    }
+
+    if(senha.length>5){
+        if(regex.test(senha)){
+            alert("ok")
+            document.getElementById("errsenha").style.display="none";
+            document.getElementById("errsenha1").style.display="none";
+            document.getElementById("password").style.border="";
+        }
+        else{
+            document.getElementById("errsenha").style.display="none";
+            document.getElementById("errsenha1").style.display="block"
+            document.getElementById("password").style.border="2px solid rgba(211, 67, 67, 0.729)";
+        }
+    }
+    else{
+        alert("aaaa")
+        document.getElementById("errsenha1").style.display="none";
+        document.getElementById("errsenha").style.display="block"
+        document.getElementById("password").style.border="2px solid rgba(211, 67, 67, 0.729)";
     }
 
 }
